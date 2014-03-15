ed7348cdf16fdd34f4ed6ad59213e91daeffcd9d
mLuby <michael@mluby.com>
2014-03-14 20:09:28 -0400

    Prettified post-POST and did regex number validation.

--- sendsms.js
+++ sendsms.js
@@ -6,30 +6,58 @@ $(document).ready(function() {
 		$("input#message").val("test v"+Math.round(Date.now()/1000%1000,0));
 		$("input#number").val("9785052128");
 	}
+
+	// If user hits [enter], it will click submit.
+	$("input").keypress(function(event) {
+	    if (event.which == 13) {
+	        // event.preventDefault();
+	        $("#button").click();
+	    }
+	});
+
 	// When submit button is pressed...
 	$("#button").click(function() {
 		// Collect inputs...
 		console.log("User clicked submit.");
 		message = $("input#message").val();
 		number = $("input#number").val();
-		// Validate values...
-		validated = true;
-		if(message.length == 0){
-			validated = false;
-			$('input#message').attr("placeholder","Type your message here.");
-			$("input#message").css("border","thin solid red");
+		// Validate message...
+		message_validated = false;
+		if(message.length > 0){
+			message_validated = true;
 		}
-		if(number.length == 0){
-			$('input#number').attr("placeholder","Enter receipient's cell number here.");
-			validated = false;
+		else{
+			// $('input#message').attr("placeholder","Type your message here.");
+			$("input#message").css("border","thin solid red");		
+		}
+		// Validate message...
+		number_validated = false;
+		if( !(/^[0-9 +()-.]+$/.test(number)) ){ //regex checks if number consists of only digits, spaces, and +()-.
+			console.log(number);
+		}
+		console.log('old number: '+number);
+		number = number.replace(/[^0-9]/g,'')
+		console.log('new number: '+number);
+		if(number.length == 10){
+			// $('input#number').attr("placeholder","Improper cell number format.");
+			number_validated = true;
 			$("input#number").css("border","thin solid red");
 		}
-		else if(number.length < 10){
-			$('input#number').attr("placeholder","Improper cell number format.");
-			validated = false;
+		else if(number.length == 11 && number[0] == 1){ // if number has a leading 1, remove it. 
+			number = number.slice(1);
+			number_validated = true;
+			console.log('number w/o leading 1: '+number);
+		}
+		else{
+			// $('input#number').attr("placeholder","Improper cell number format.");
 			$("input#number").css("border","thin solid red");
 		}
-		if(validated){
+
+		// if any chararacters other than integers and "+()-. " exist in number, ask user for proper formatting.
+		// else if after stripping all non-numeric characters from number, there are fewer than 10 characters, complain.
+		// else validate number.
+
+		if(message_validated && number_validated){
 			// POST to the API...
 			$.ajax({
 				type: "POST",
@@ -39,16 +67,19 @@ $(document).ready(function() {
 					console.log(message+" -> "+number+":"+JSON.stringify(info));
 					// And inform user of success.
 					$("#send").text("Successfully texted");
-					$("input#message").replaceWith("<span class=\"send_sentence\">\""+message+"\"</span>");
-					$("input#number").replaceWith("<span class=\"send_sentence\">"+number+".</span>");
-					$("#button").remove();
-					$(".send_sentence").css("color", "green");
+					$("input#message").replaceWith("<span class=\"sentence\">\""+message+"\"</span>");
+					$("input#number").replaceWith("<span class=\"sentence\">"+number+".</span>");
+					$("#button").replaceWith("<span class=\"sentence\">Texted</span>");
+					$('h1').replaceWith("<h1>Sent SMS. <a href=\"\">Another?</a></h1>");
 				},
 				error: function(info) {
 					// Or log error if POST fails.
 					console.log('Ajax POST failed:'+JSON.stringify(info));
 				}
 			});
+		}
+		else{
+			console.log("Inputs did not pass validation.")
 		}
 	});
 });
